ffeb3911f9ba8844bb4e30229fa36eedbbef03d1
MinaEzz <minaezzat89@gmail.com>
2023-09-07 14:12:39 +0300

    search updated

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,46 +1,56 @@
-import React, { useState } from 'react'
-import Navbar from '../Navbar/Nav_bar'
-import SectionHeader from '../header/Section_header'
-import CardItem from '../card item/Card_item'
-// data 
-import { meals } from '../../meals'
+import React, { useState } from "react";
+import Navbar from "../Navbar/Nav_bar";
+import SectionHeader from "../header/Section_header";
+import CardItem from "../card item/Card_item";
+// data
+import { meals } from "../../meals";
 
 const Main = (props) => {
   // meal state
-  const [mealItem, setMealItem] = useState(meals)
+  const [mealItem, setMealItem] = useState(meals);
   // meals filteration
-  const categoryFilter = (cat)=>{
-    if (cat === "all") 
-    {
-      setMealItem(meals)
-    } 
-    else{
-      const filteredMeals = meals.filter((meal)=> meal.category === cat)
-      setMealItem(filteredMeals)
+  const categoryFilter = (cat) => {
+    if (cat === "all") {
+      setMealItem(meals);
+    } else {
+      const filteredMeals = meals.filter((meal) => meal.category === cat);
+      setMealItem(filteredMeals);
     }
-  }
+  };
   // extract categories of meals and move it to new array called categories
-  const categories = ["all", ...new Set(meals.map((meal)=>{return(meal.category)}))] ;
+  const categories = [
+    "all",
+    ...new Set(
+      meals.map((meal) => {
+        return meal.category;
+      })
+    ),
+  ];
   // search
-  const search = (word)=>{
-    if(word !== ""){
-      const searchedMeals = meals.filter((meal)=> meal.title === word)
-      setMealItem(searchedMeals)
+  const search = (word) => {
+    if (word !== "") {
+      const searchedMeals = meals.filter(
+        (meal) => meal.title === word.toLowerCase()
+      );
+      setMealItem(searchedMeals);
     }
-  }
+  };
   return (
-    <main className='pd-y f-wth'>
-        <Navbar searchFunction = {search} />
+    <main className="pd-y f-wth">
+      <Navbar searchFunction={search} />
       <div className="container">
-      <SectionHeader categories = {categories} categoryFilterFunction = {categoryFilter} />
-      <div className="items mt-5">
-        <div className='row'>
-        <CardItem meals = {mealItem} />
+        <SectionHeader
+          categories={categories}
+          categoryFilterFunction={categoryFilter}
+        />
+        <div className="items mt-5">
+          <div className="row">
+            <CardItem meals={mealItem} />
+          </div>
         </div>
       </div>
-      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
